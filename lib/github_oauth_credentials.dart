d9769994970fd80156f3664662da733e9aa8e089
kunalkharat22 <kunalkharat22@users.noreply.github.com>
2021-02-16 10:33:13 +0530

    First changes

---
+++ lib/github_oauth_credentials.dart
@@ -0,0 +1,6 @@
+// TODO(CodelabUser): Create an OAuth App
+const githubClientId = '5f07955bd1e5f5e08e19';
+const githubClientSecret = 'a4d7326d3cf3672058f3b523b2c8071acf7a012e';
+
+// OAuth scopes for repository and user information
+const githubScopes = ['repo', 'read:org'];
